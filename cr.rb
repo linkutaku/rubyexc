6d0e51131c92a20c9ff850f0147ca1b36fd6382e
linkutaku <linkinp1125@gmail.com>
2016-10-18 23:56:16 -0700

    rubyexc

--- cr.rb
+++ cr.rb
@@ -2,15 +2,14 @@ h = ARGV[0].to_i
 f = ARGV[1].to_i
 c = 0
 while c <= h do
-	r = h - c
-	if c * 2 + r * 4 == f 
-		break
-	end
-	c += 1
+  r = h - c
+  break if c * 2 + r * 4 == f 
+  end
+  c += 1
 end
 if c <= h
-	puts "Chicken: #{c}"
-	puts "Rabbit: #{r}"
+  puts "Chicken: #{c}"
+  puts "Rabbit: #{r}"
 else
-	puts "no result"
+  puts "no result"
 end
